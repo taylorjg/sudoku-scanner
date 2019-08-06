952d585368435a0cc91c6e347d6771491954b177
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-08-06 17:42:38 +0100

    Extract digits from training data images ready for digit training

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import * as tf from '@tensorflow/tfjs'
 import * as tfvis from '@tensorflow/tfjs-vis'
 import * as R from 'ramda'
 import axios from 'axios'
+import puzzles from '../data/puzzles.json'
 import trainingData from '../data/training-data.json'
 import validationData from '../data/validation-data.json'
 import testData from '../data/test-data.json'
 
-let model = undefined
-let trained = false
+let gridModel = undefined
+let digitsModel = undefined
+let trainedGrid = false
+let trainedDigits = false
 let imageData = undefined
 
 const IMAGE_WIDTH = 224
@@ -17,17 +20,25 @@ const IMAGE_CHANNELS = 1
 const inset = (x, y, w, h, dx, dy) =>
   [x + dx, y + dy, w - 2 * dx, h - 2 * dy]
 
-const drawDigitBoxes = (ctx, boundingBox, colour) => {
+function* calculateDigitBoxes(boundingBox) {
   const [bbx, bby, bbw, bbh] = boundingBox
-  const digitw = bbw / 9
-  const digith = bbh / 9
-  for (const col of R.range(0, 9)) {
-    const x = bbx + col * digitw
-    for (const row of R.range(0, 9)) {
-      const y = bby + row * digith
-      ctx.strokeStyle = colour
-      ctx.strokeRect(...inset(x, y, digitw, digith, digitw / 10, digith / 10))
+  const w = bbw / 9
+  const h = bbh / 9
+  const dx = w / 10
+  const dy = h / 10
+  for (const row of R.range(0, 9)) {
+    const y = bby + row * h
+    for (const col of R.range(0, 9)) {
+      const x = bbx + col * w
+      yield inset(x, y, w, h, dx, dy)
     }
+  }
+}
+
+const drawDigitBoxes = (ctx, boundingBox, colour) => {
+  for (const digitBox of calculateDigitBoxes(boundingBox)) {
+    ctx.strokeStyle = colour
+    ctx.strokeRect(...digitBox)
   }
 }
 
@@ -46,6 +57,17 @@ const drawImageTensor = async (imageTensor, boundingBoxTarget, boundingBoxPredic
   }
   const body = document.querySelector('body')
   body.appendChild(canvas)
+}
+
+const drawDigitImageTensors = t4d => {
+  const body = document.querySelector('body')
+  const t3ds = tf.unstack(t4d)
+  t3ds.forEach(async t3d => {
+    const canvas = document.createElement('canvas')
+    canvas.setAttribute('style', 'margin: 2px')
+    await tf.browser.toPixels(t3d, canvas)
+    body.appendChild(canvas)
+  })
 }
 
 // const drawImageTensors = (trainData, imageTensors) => {
@@ -137,10 +159,10 @@ const loadImage = async url => {
 
 // const loadTrainingData = () => loadData(trainingData)
 
-const BATCH_SIZE = 100
+const GRID_DATA_BATCH_SIZE = 1000
 
-async function* dataGenerator(data) {
-  const batches = R.splitEvery(BATCH_SIZE, data)
+async function* gridDataGenerator(data) {
+  const batches = R.splitEvery(GRID_DATA_BATCH_SIZE, data)
   for (const batch of batches) {
     const urls = batch.map(item => item.url)
     const promises = urls.map(loadImage)
@@ -155,7 +177,42 @@ async function* dataGenerator(data) {
   }
 }
 
-const createModel = () => {
+const digitDataGenerator = async data => {
+  const urls = data.map(item => item.url)
+  const promises = urls.map(loadImage)
+  const imageTensors = await Promise.all(promises)
+  imageTensors.forEach((imageTensor, index) => {
+    const { boundingBox, puzzleId } = data[index]
+    const puzzle = puzzles.find(p => p.id === puzzleId)
+    const initialValues = Array.from(puzzle.initialValues.join(''))
+    const digits = initialValues
+      .map((initialValue, index) => ({ digit: Number(initialValue), index }))
+      .filter(({ digit }) => Number.isInteger(digit) && digit >= 1 && digit <= 9)
+    const allDigitBoxes = Array.from(calculateDigitBoxes(boundingBox))
+    const digitBoxes = digits
+      .map(({ digit, index }) => ({ digit, digitBox: allDigitBoxes[index] }))
+    const image = tf.stack([imageTensor.toFloat().div(255)])
+    const boxes = digitBoxes.map(({ digitBox }) => {
+      const [x, y, w, h] = digitBox
+      const y1 = y
+      const x1 = x
+      const y2 = y1 + h
+      const x2 = x1 + w
+      return [
+        y1 / (IMAGE_HEIGHT - 1),
+        x1 / (IMAGE_WIDTH - 1),
+        y2 / (IMAGE_HEIGHT - 1),
+        x2 / (IMAGE_WIDTH - 1)
+      ]
+    })
+    const boxInd = Array(boxes.length).fill(0)
+    const cropSize = [20, 20]
+    const digitImageTensors = tf.image.cropAndResize(image, boxes, boxInd, cropSize)
+    drawDigitImageTensors(digitImageTensors)
+  })
+}
+
+const createGridModel = () => {
   const inputShape = [IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS]
   const conv2dArgs = {
     kernelSize: 3,
@@ -189,14 +246,17 @@ const createModel = () => {
   return model
 }
 
-const train = async model => {
+const createDigitsModel = () => {
+}
+
+const trainGrid = async model => {
   model.compile({
     optimizer: 'rmsprop',
     loss: 'meanSquaredError'
     // loss: 'meanAbsoluteError'
   })
-  const trainingDataset = tf.data.generator(() => dataGenerator(trainingData))
-  const validationDataset = tf.data.generator(() => dataGenerator(validationData))
+  const trainingDataset = tf.data.generator(() => gridDataGenerator(trainingData))
+  const validationDataset = tf.data.generator(() => gridDataGenerator(validationData))
 
   const trainingSurface = tfvis.visor().surface({ tab: 'Tab 1', name: 'Model Training' })
   const customCallback = tfvis.show.fitCallbacks(
@@ -212,6 +272,9 @@ const train = async model => {
     callbacks: customCallback
   }
   return model.fitDataset(trainingDataset, params)
+}
+
+const trainDigits = async model => {
 }
 
 const createSvgElement = (elementName, additionalAttributes = {}) => {
@@ -304,7 +367,7 @@ const initialiseCamera = async () => {
     const canvas = document.createElement('canvas')
     canvas.width = IMAGE_WIDTH
     canvas.height = IMAGE_HEIGHT
-    const imageTensor = normaliseImage(imageData)
+    const imageTensor = normaliseGridImage(imageData)
     await tf.browser.toPixels(imageTensor, canvas)
     const dataUrlNormalised = canvas.toDataURL('image/png')
     const responseNormalised = await axios.post('/api/saveNormalisedImage', { dataUrl: dataUrlNormalised })
@@ -327,31 +390,49 @@ const initialiseCamera = async () => {
   updateButtonState()
 }
 
-const onTrain = async () => {
+const onTrainGrid = async () => {
   try {
-    trainBtn.disabled = true
-    model = createModel()
-    const trainingResults = await train(model)
+    trainGridBtn.disabled = true
+    gridModel = createGridModel()
+    const trainingResults = await trainGrid(gridModel)
     console.dir(trainingResults)
     const lastLoss = R.last(trainingResults.history.loss)
     const lastValLoss = R.last(trainingResults.history.val_loss)
     console.log('last loss:', lastLoss, 'sqrt:', Math.sqrt(lastLoss))
     console.log('last val_loss:', lastValLoss, 'sqrt:', Math.sqrt(lastValLoss))
-    trained = true
-    predictTestDataBtn.disabled = false
+    trainedGrid = true
+    predictGridTestDataBtn.disabled = false
   } finally {
-    trainBtn.disabled = false
+    trainGridBtn.disabled = false
   }
 }
 
-const normaliseImage = imageData => {
+const onTrainDigits = async () => {
+  try {
+    trainDigitsBtn.disabled = true
+    await digitDataGenerator(trainingData)
+    // digitsModel = createDigitsModel()
+    // const trainingResults = await trainDigits(digitsModel)
+    // console.dir(trainingResults)
+    // const lastLoss = R.last(trainingResults.history.loss)
+    // const lastValLoss = R.last(trainingResults.history.val_loss)
+    // console.log('last loss:', lastLoss, 'sqrt:', Math.sqrt(lastLoss))
+    // console.log('last val_loss:', lastValLoss, 'sqrt:', Math.sqrt(lastValLoss))
+    // trainedDigits = true
+    // predictDigitsTestDataBtn.disabled = false
+  } finally {
+    trainDigitsBtn.disabled = false
+  }
+}
+
+const normaliseGridImage = imageData => {
   const imageDataGreyscale = convertToGreyscale(imageData)
   const imageTensorGreyscale = tf.browser.fromPixels(imageDataGreyscale, IMAGE_CHANNELS)
   return tf.image.resizeBilinear(imageTensorGreyscale, [IMAGE_HEIGHT, IMAGE_WIDTH])
 }
 
 const onPredictCapture = async () => {
-  const imageTensor = normaliseImage(imageData)
+  const imageTensor = normaliseGridImage(imageData)
   // const input = tf.stack([imageTensor])
   // const output = model.predict(input)
   // const boundingBoxPrediction = output.arraySync()[0]
@@ -360,11 +441,11 @@ const onPredictCapture = async () => {
   drawImageTensor(imageTensor, undefined, undefined)
 }
 
-const onPredictTestData = async () => {
+const onPredictGridTestData = async () => {
   const promises = testData.map(item => item.url).map(loadImage)
   const imageTensors = await Promise.all(promises)
   const input = tf.stack(imageTensors)
-  const output = model.predict(input)
+  const output = gridModel.predict(input)
   const boundingBoxPredictions = output.arraySync()
   imageTensors.map((imageTensor, index) => {
     const boundingBoxTarget = testData[index].boundingBox
@@ -375,16 +456,26 @@ const onPredictTestData = async () => {
   })
 }
 
-const trainBtn = document.getElementById('trainBtn')
-trainBtn.addEventListener('click', onTrain)
+const onPredictDigitsTestData = async () => {
+}
+
+const trainGridBtn = document.getElementById('trainGridBtn')
+trainGridBtn.addEventListener('click', onTrainGrid)
+
+const trainDigitsBtn = document.getElementById('trainDigitsBtn')
+trainDigitsBtn.addEventListener('click', onTrainDigits)
 
 const predictCaptureBtn = document.getElementById('predictCaptureBtn')
 predictCaptureBtn.addEventListener('click', onPredictCapture)
 predictCaptureBtn.disabled = true
 
-const predictTestDataBtn = document.getElementById('predictTestDataBtn')
-predictTestDataBtn.addEventListener('click', onPredictTestData)
-predictTestDataBtn.disabled = true
+const predictGridTestDataBtn = document.getElementById('predictGridTestDataBtn')
+predictGridTestDataBtn.addEventListener('click', onPredictGridTestData)
+predictGridTestDataBtn.disabled = true
+
+const predictDigitsTestDataBtn = document.getElementById('predictDigitsTestDataBtn')
+predictDigitsTestDataBtn.addEventListener('click', onPredictDigitsTestData)
+predictDigitsTestDataBtn.disabled = true
 
 const main = async () => {
   drawGuides()
